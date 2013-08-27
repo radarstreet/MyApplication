f7e009db9f450d681a707c2fe0233fb26763c232
Radarstreet <darstra@gmail.com>
2013-08-26 19:30:12 -0500

    Initial commit

---
+++ MyApplication/build/source/buildConfig/debug/com/radarstreet/android/myapplication/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.radarstreet.android.myapplication;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
